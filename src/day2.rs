d714d2e85c23724d9799ddff25fa0fd8cd724c28
NeunEinser <daniel30797@gmail.com>
2023-12-02 22:13:25 +0100

    Part 2 of day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,22 +1,35 @@
-use std::{process, fs};
+use std::{process, fs, cmp};
 
 use crate::UnwrapOrExit;
 
+struct CubeCounts {
+	red: u32,
+	green: u32,
+	blue: u32
+}
+
 pub fn main(args: Vec<String>) {
 	
-	let syntax = format!("Syntax: {} {} <file path> <max_red> <max_green> <max_blue>", args[0], args[1]);
+	let syntax = format!("Syntax: {} {} <file path> [<max_red> <max_green> <max_blue>]", args[0], args[1]);
 
-	if args.len() < 6 {
+	if args.len() != 3 && args.len() != 6 {
 		eprintln!("{syntax}");
 		process::exit(1);
 	}
 
 	let content = fs::read_to_string(&args[2]).unwrap_or_exit("Could not read file content as Utf-8 string", 1);
-	let max_red = args[3].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_red>, expected number, got {}", args[3]), 1);
-	let max_green = args[4].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_green>, expected number, got {}", args[4]), 1);
-	let max_blue = args[5].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_blue>, expected number, got {}", args[5]), 1);
+	let max = if args.len() == 3 {
+		[0; 3]
+	} else {
+		[ 
+			args[3].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_red>, expected number, got {}", args[3]), 1),
+			args[4].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_green>, expected number, got {}", args[4]), 1),
+			args[5].parse::<u32>().unwrap_or_exit(&format!("{syntax}\nCould not read <max_blue>, expected number, got {}", args[5]), 1)
+		]
+	};
 
-	let mut result = 0;
+	let mut possible_sum = 0;
+	let mut power_sum = 0;
 
 	for line in content.lines() {
 		let line = line.trim();
@@ -30,19 +43,27 @@ pub fn main(args: Vec<String>) {
 		let game_id_length = line.find(":").unwrap_or_exit(&err, 1);
 		let game_id = String::from(&line[..game_id_length]).parse::<u32>().unwrap_or_exit(&err, 1);
 
-		if parse_line(line[game_id_length + 1..].trim(), max_red, max_green, max_blue, &err) {
-			println!("Game {game_id} is possible!");
-			result += game_id;
-		} else {
-			println!("Game {game_id} is impossible!");
+		let counts = get_min_cubes(line[game_id_length + 1..].trim()).unwrap_or_exit(&err, 1);
+
+		power_sum += counts.red * counts.green * counts.blue;
+		if counts.red <= max[0] && counts.green <= max[1] && counts.blue <= max[2] {
+			possible_sum += game_id;
 		}
 	}
 
-	println!("The sum of possible game IDs is {result}");
+	if args.len() == 6 {
+		println!("The sum of possible game IDs is {possible_sum}");
+	}
+	println!("The sum of powers is {power_sum}");
 }
 
-fn parse_line(line: &str, max_red: u32, max_green: u32, max_blue: u32, err: &str) -> bool {
+fn get_min_cubes(line: &str) -> Result<CubeCounts, String> {
 	let mut line = line;
+	let mut counts = CubeCounts {
+		red: 0,
+		green: 0,
+		blue: 0
+	};
 
 	while !line.is_empty() {
 		let subset_len = line.find(";").unwrap_or(line.len());
@@ -52,24 +73,19 @@ fn parse_line(line: &str, max_red: u32, max_green: u32, max_blue: u32, err: &str
 			let len = subset.find(",").unwrap_or(subset.len());
 			let item = subset[..len].trim();
 
-			let num_length = item.find(|c: char| !c.is_ascii_digit()).unwrap_or_exit(&err, 1);
-			let num = String::from(&item[..num_length]).parse::<u32>().unwrap_or_exit(&err, 1);
+			let num_length = item.find(|c: char| !c.is_ascii_digit()).ok_or(String::from("invalid cube count"))?;
+			let num = String::from(&item[..num_length]).parse::<u32>().map_err(|err| format!("Invalid cube count: {err}"))?;
 		
 			let item = item[num_length + 1..].trim();
 
-			let impossible_amount = match item {
-				"red" => num > max_red,
-				"green" => num > max_green,
-				"blue" => num > max_blue,
+			match item {
+				"red" => counts.red = cmp::max(counts.red, num),
+				"green" => counts.green = cmp::max(counts.green, num),
+				"blue" => counts.blue = cmp::max(counts.blue, num),
 				_ => {
-					eprintln!("{err}");
-					process::exit(1);
+					return Err(format!("invalid color {item}"));
 				}
 			};
-
-			if impossible_amount {
-				return false;
-			}
 
 			if len >= subset.len() {
 				break;
@@ -83,5 +99,5 @@ fn parse_line(line: &str, max_red: u32, max_green: u32, max_blue: u32, err: &str
 		line = line[subset_len+1..].trim();
 	}
 
-	true
+	Ok(counts)
 }
