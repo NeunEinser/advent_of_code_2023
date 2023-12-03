910665df2754a9cc45a2295d639b83bc6b48c7d1
NeunEinser <daniel30797@gmail.com>
2023-12-03 09:18:03 +0100

    Add logic for day3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::{process, fs, cmp};
+use std::{process, fs, cmp, collections::HashMap};
 
 use crate::UnwrapOrExit;
 
@@ -14,39 +14,46 @@ pub fn main(args: Vec<String>) {
 
 	let content = fs::read_to_string(&args[2]).unwrap_or_exit("Could not read file content as Utf-8 string", 1);
 	let lines: Vec<&str> =  content.lines().collect();
+	let mut nodes: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
 
 	let mut sum = 0;
 	for (i, line) in lines.iter().enumerate() {
 		let mut j = line.find(|c: char| c.is_ascii_digit()).unwrap_or(line.len());
 		while !line[j..].is_empty() {
 			let num_end = line[j..].find(|c: char| !c.is_ascii_digit()).unwrap_or(line[j..].len()) + j;
-			let num = line[j..num_end].parse::<u32>().unwrap();
+			let num = line[j..num_end].parse::<u32>().expect("j should be a digit and num end should be the first non-digit, so this should always produce a valid u32");
 
 			let mut counts = false;
 			if i > 0 {
-				if lines[i-1][cmp::max(0, j as isize - 1) as usize..cmp::min(lines[i-1].len(), num_end + 1)].contains(|c: char| !c.is_ascii_digit() && c != '.') {
+				let start = cmp::max(0, j as isize - 1) as usize;
+				let end = cmp::min(lines[i-1].len(), num_end + 1);
+				if let Some(j) = lines[i-1][start..end].find(|c: char| !c.is_ascii_digit() && c != '.') {
 					counts = true;
+					add_node(&mut nodes, &lines, i - 1, j + start, num);
 				}
 			}
 				
 			if j > 0 && !line.as_bytes()[j-1].is_ascii_digit() && line.as_bytes()[j-1] != b'.' {
-				counts = true
+				counts = true;
+				add_node(&mut nodes, &lines, i, j - 1, num);
 			}
 			
 			if num_end + 1 < line.len() && line.as_bytes()[num_end] != b'.' {
-				println!("{}", line.as_bytes()[num_end] as char);
-				counts = true
+				counts = true;
+				add_node(&mut nodes, &lines, i, num_end, num);
 			}
 
 			if i + 2 < lines.len() {
-				if lines[i+1][cmp::max(0, j as isize - 1) as usize..cmp::min(lines[i+1].len(), num_end + 1)].contains(|c: char| !c.is_ascii_digit() && c != '.') {
+				let start = cmp::max(0, j as isize - 1) as usize;
+				let end = cmp::min(lines[i+1].len(), num_end + 1);
+				if let Some(j) = lines[i+1][start..end].find(|c: char| !c.is_ascii_digit() && c != '.') {
 					counts = true;
+					add_node(&mut nodes, &lines, i + 1, j + start, num);
 				}
 			}
 
 			if counts {
 				sum+=num;
-				//println!("{}", &line[j..num_end]);
 			}
 
 			j = num_end;
@@ -54,5 +61,25 @@ pub fn main(args: Vec<String>) {
 		}
 	}
 
-	println!("{sum}")
+	let mut gear_sum = 0;
+	for (_, v) in nodes {
+		if v.len() == 2 {
+			gear_sum += v[0] * v[1];
+		}
+	}
+
+	println!("Sum of part numbers: {sum}");
+	println!("Sum of gear ratios: {gear_sum}");
+}
+
+fn add_node(nodes: &mut HashMap<(usize, usize), Vec<u32>>, lines: &[&str], i: usize, j : usize, num: u32) {
+	if lines[i].as_bytes()[j] != b'*' {
+		return;
+	}
+
+	if !nodes.contains_key(&(i, j)) {
+		nodes.insert((i, j), Vec::new());
+	}
+	let v = nodes.get_mut(&(i, j)).expect("Should be inserted the line before");
+	v.push(num);
 }
