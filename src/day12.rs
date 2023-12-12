9410921fb7bcc13ae2c1f5a117f554dedf9e25e5
NeunEinser <daniel30797@gmail.com>
2023-12-13 00:05:14 +0100

    Adjust for part2 and optimize
    Part2 requires to duplicate each line 5 times.
    Doing that naivly, the program does not terminate within reasonable
    time.
    
    The main performance improvement here is to use a HashMap to cache
    results, so when we iterate the tree and hit a state that we had
    previously, we do not re-compute everything.
    
    Furthermore, I also skip sequences of operational springs. This did not
    seem to improve performance much but still should give some
    improvements.

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,4 @@
-use std::{process, fs};
+use std::{process, fs, iter, collections::HashMap};
 
 use crate::UnwrapOrExit;
 /// https://adventofcode.com/2023/day/12
@@ -15,21 +15,31 @@ pub fn main(args: Vec<String>) {
 	let mut sum = 0;
 	for line in content.lines() {
 		let (springs, nums) = line.split_once(' ').unwrap_or_exit(&format!("Could not parse line {line}"), 1);
-		let springs: Vec<_> = springs.chars().map(|c| match c {
-			'.' => SpringState::Operational,
-			'#' => SpringState::Damaged,
-			_ => SpringState::Unknown
-		}).collect();
-		let nums: Result<Vec<usize>, _> = nums.split(',').map(str::parse).collect();
+		let springs: Vec<_> = iter::repeat(
+			springs.chars().map(|c| match c {
+				'.' => SpringState::Operational,
+				'#' => SpringState::Damaged,
+				_ => SpringState::Unknown
+			}))
+			.take(5)
+			.flat_map(|i| i.chain(iter::once(SpringState::Unknown)))
+			.take((springs.len()+1) * 5 - 1)
+			.collect();
+
+		let nums: Result<Vec<usize>, _> = iter::repeat(
+			nums.split(',').map(str::parse))
+			.take(5)
+			.flatten()
+			.collect();
 		let nums = nums.unwrap_or_exit(&format!("Could not parse num section of line {line}"), 1);
 
-		sum += get_arrangements(&springs, &nums);
+		sum += get_arrangements(&springs, &nums, &mut HashMap::new());
 	}
 
 	println!("{sum}");
 }
 
-fn get_arrangements(springs: &[SpringState], nums: &[usize]) -> u32 {
+fn get_arrangements<'a, 'b>(springs: &'a [SpringState], nums: &'b [usize], cache: &mut HashMap<(&'a [SpringState], &'b[usize]), u64>) -> u64 {
 	if springs.is_empty() || nums.is_empty() {
 		return 0;
 	}
@@ -42,7 +52,16 @@ fn get_arrangements(springs: &[SpringState], nums: &[usize]) -> u32 {
 		&& (springs.len() == nums[0] || springs[nums[0]] == SpringState::Operational || springs[nums[0]] == SpringState::Unknown) {
 			arrangements += if nums.len() > 1 {
 				if springs.len() > nums[0]+1 {
-					get_arrangements(&springs[nums[0]+1..], &nums[1..])
+					// This would be nicer as cache.entry(...).or_insert_with(...), but the borrow checker cries there
+					// because the recursive call requires the cache, borrowing the cache mutable twice.
+					let key = (get_slice_with_first_non_operational_after(springs, nums[0]+1), &nums[1..]);
+					if cache.contains_key(&key) {
+						cache[&key]
+					} else {
+						let result = get_arrangements(key.0, key.1, cache);
+						cache.insert(key, result);
+						result
+					}
 				} else {
 					0
 				}
@@ -56,15 +75,20 @@ fn get_arrangements(springs: &[SpringState], nums: &[usize]) -> u32 {
 		if springs[0] == SpringState::Damaged {
 			break;
 		}
-		springs = &springs[1..];
+		springs = get_slice_with_first_non_operational_after(springs, 1);
 	}
 
 	return arrangements;
 }
 
+fn get_slice_with_first_non_operational_after(slice: &[SpringState], after: usize) -> &[SpringState] {
+	&slice[slice.iter().skip(after).position(|s| *s != SpringState::Operational).unwrap_or(slice.len()-after)+after..]
+}
+
+#[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Hash)]
 enum SpringState {
-	Unknown,
-	Operational,
-	Damaged,
+	Unknown = b'?',
+	Operational = b'.',
+	Damaged = b'#',
 }
